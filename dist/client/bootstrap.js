8820b3979be78eb24cd7e8caae7fff8b40ce89b0
Lancer3D <lancer3d@Lancer3Ds-MacBook-Air.local>
2026-08-31 16:54:05 +0300

    Improve front-view static stretch anatomy

--- dist/client/bootstrap.js
+++ dist/client/bootstrap.js
@@ -1,5 +1,5 @@
 (async function(){
-  const version=window.REP_BUILD_VERSION||"3e2f6ace6969";
+  const version=window.REP_BUILD_VERSION||"be3ed17fd9a3";
   const load=src=>new Promise((resolve,reject)=>{
     const script=document.createElement("script");
     script.src=`${src}?v=${version}`;
